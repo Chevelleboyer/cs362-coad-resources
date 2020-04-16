86b7007c3407c3c447698b7f1dc53a40eff45147
Wyatt Pierce <piercewy@oregonstate.edu>
2020-04-16 09:32:28 -0700

    Added name attribute test for organization

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Organization, type: :model do
   it "exists" do
     Organization.new()
   end
+
+  it "checks for a name attribute" do
+    expect(Organization).to respond_to(:name)
+  end
 end
