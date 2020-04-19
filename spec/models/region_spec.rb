03033b154b55d31bb500c3c798ffb5d364c40476
Wyatt Pierce <piercewy@oregonstate.edu>
2020-04-19 16:28:18 -0700

    Added characterization tests for Region

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Region, type: :model do
   it "exists" do
     Region.new()
   end
+
+  it "has all of the expected attributes" do
+    region = Region.new()
+    expect(region).to respond_to(:name)
+  end
 end
