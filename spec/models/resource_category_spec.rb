b5f303099dad45f0e7dc8abb318b6c9aedad2ba8
Wyatt Pierce <piercewy@oregonstate.edu>
2020-04-19 16:30:51 -0700

    Added characterization tests for ResourceCategory

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe ResourceCategory, type: :model do
   it "exists" do
     ResourceCategory.new()
   end
+
+  it "has all of the expected attributes" do
+    resource_category = ResourceCategory.new()
+    expect(resource_category).to respond_to(:name)
+  end
 end
