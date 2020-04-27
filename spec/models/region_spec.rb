8effc969843e4f4a954c6a64dc49ce479fa8c4b0
Wyatt Pierce <piercewy@oregonstate.edu>
2020-04-26 17:55:55 -0700

    Remove instantiation test for region

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Region, type: :model do
-  it "exists" do
-    Region.new()
-  end
 
   it "has all of the expected attributes" do
     region = Region.new()
     expect(region).to respond_to(:name)
   end
+  
 end
