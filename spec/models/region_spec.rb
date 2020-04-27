058865e8121403f4576107c122f2602537ad65a0
Wyatt Pierce <piercewy@oregonstate.edu>
2020-04-26 18:02:24 -0700

    Add association tests for Region

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
-  it "has all of the expected attributes" do
-    region = Region.new()
+  let(:region) {Region.new()}
+
+  it "has a name" do
     expect(region).to respond_to(:name)
   end
-  
+
+  describe "relationships" do
+    it { should have_many(:tickets) }
+  end
+
 end
