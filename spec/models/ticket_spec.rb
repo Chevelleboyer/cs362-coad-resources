a6f0483616acd9b1f87bcc2ab1b4a7df0ea1546d
Wyatt Pierce <piercewy@oregonstate.edu>
2020-04-26 17:07:18 -0700

    Refactor attribute tests for Ticket

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -4,12 +4,10 @@ RSpec.describe Ticket, type: :model do
 
   it "has all of the expected attributes" do
     ticket = Ticket.new()
-    expect(ticket).to respond_to(
-      :name,
-      :phone,
-      :region_id,
-      :resource_category_id
-    )
+    expect(ticket).to respond_to(:name)
+    expect(ticket).to respond_to(:phone)
+    expect(ticket).to respond_to(:region_id)
+    expect(ticket).to respond_to(:resource_category_id)
     
   end
 
