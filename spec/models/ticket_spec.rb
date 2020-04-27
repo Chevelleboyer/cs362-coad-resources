ba71f517e460e0258e6874be25919ff9e3b22dfc
Wyatt Pierce <piercewy@oregonstate.edu>
2020-04-26 17:19:36 -0700

    Give each attribute test in Ticket its own it block

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,13 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
-  it "has all of the expected attributes" do
+  it "has a name" do
     ticket = Ticket.new()
     expect(ticket).to respond_to(:name)
+  end
+
+  it "has a phone" do
+    ticket = Ticket.new()
     expect(ticket).to respond_to(:phone)
+  end
+
+  it "has a region_id" do
+    ticket = Ticket.new()
     expect(ticket).to respond_to(:region_id)
+  end
+
+  it "has a resource_category_id" do
+    ticket = Ticket.new()
     expect(ticket).to respond_to(:resource_category_id)
-    
   end
 
 end
