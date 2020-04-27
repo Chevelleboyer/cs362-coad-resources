fc7881ccbec646c47c5f1e0fb9ff089eee61a75a
Wyatt Pierce <piercewy@oregonstate.edu>
2020-04-26 18:06:13 -0700

    Move object instantiation to let

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
-  it "has all of the expected attributes" do
-    resource_category = ResourceCategory.new()
+  let(:resource_category) {ResourceCategory.new()}
+
+  it "has all of the expected attributes" do 
     expect(resource_category).to respond_to(:name)
   end
-  
+
 end
