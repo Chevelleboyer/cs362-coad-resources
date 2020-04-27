0da7efa947308253e03db45e3aa20b2a1eba9d12
Wyatt Pierce <piercewy@oregonstate.edu>
2020-04-26 18:04:50 -0700

    Remove instantiation test for resource category

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
-  it "exists" do
-    ResourceCategory.new()
-  end
 
   it "has all of the expected attributes" do
     resource_category = ResourceCategory.new()
     expect(resource_category).to respond_to(:name)
   end
+  
 end
