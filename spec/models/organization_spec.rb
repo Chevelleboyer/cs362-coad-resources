391a570e7c827b7f18876085dd13bd6eba15c386
Chevelle Boyer <chevelleboyer@gmail.com>
2020-05-04 13:36:15 -0700

    Add specs for organization validations

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -103,3 +103,58 @@ RSpec.describe Organization, type: :model do
 			end
 
 	end
+
+	describe "validations" do
+
+		it "validates presence of email" do
+			expect(organization).to validate_presence_of(:email)
+		end
+
+		it "validates presence of name" do
+			expect(organization).to validate_presence_of(:name)
+		end
+
+		it "validates presence of phone" do
+			expect(organization).to validate_presence_of(:phone)
+		end
+
+		it "validates presence of status" do
+			expect(organization).to validate_presence_of(:status)
+		end
+
+		it "validates presence of primary_name" do
+			expect(organization).to validate_presence_of(:primary_name)
+		end
+
+		it "validates presence of secondary_name" do
+			expect(organization).to validate_presence_of(:secondary_name)
+		end
+
+		it "validates presence of secondary_phone" do
+			expect(organization).to validate_presence_of(:secondary_phone)
+		end
+		
+		it "validates length of email" do
+			expect(organization).to validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create)
+		end
+
+		it "validates uniqueness of email" do
+			expect(organization).to validate_uniqueness_of(:email).case_insensitive
+		end
+
+		it "validates length of name" do
+			expect(organization).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
+		end
+
+		it "validates uniqueness of name" do
+			expect(organization).to validate_uniqueness_of(:name).case_insensitive
+		end
+
+		it "validates length of description" do
+			expect(organization).to validate_length_of(:description).is_at_most(1020).on(:create)
+		end
+
+	end
+
+end
+
