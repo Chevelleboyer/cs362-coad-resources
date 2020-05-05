8503683a85cc2203d44f5bddbf3cd999de7daab5
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-05 09:11:25 -0700

    Add method test for open in ticket spec

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -4,26 +4,37 @@ RSpec.describe Ticket, type: :model do
 
   let(:ticket) {build(:ticket)}
 
-  it "has a name" do
-    expect(ticket).to respond_to(:name)
+  describe "validations" do
+    it "has a name" do
+      expect(ticket).to respond_to(:name)
+    end
+
+    it "has a phone" do
+      expect(ticket).to respond_to(:phone)
+    end
+
+    it "has a region_id" do
+      expect(ticket).to respond_to(:region_id)
+    end
+
+    it "has a resource_category_id" do
+      expect(ticket).to respond_to(:resource_category_id)
+    end
   end
 
-  it "has a phone" do
-    expect(ticket).to respond_to(:phone)
-  end
-
-  it "has a region_id" do
-    expect(ticket).to respond_to(:region_id)
-  end
-
-  it "has a resource_category_id" do
-    expect(ticket).to respond_to(:resource_category_id)
-  end
-
-  describe "ticket relationships" do
+  describe "relationships" do
     it { should belong_to(:region) }
     it { should belong_to(:resource_category) }
     it { should belong_to(:organization) }
   end
+
+  describe "methods" do
+    it "is open by default" do
+      expect(ticket.open?).to be true
+    end
+
+  end
+
+
 
 end
