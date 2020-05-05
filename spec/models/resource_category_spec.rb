e02d34fdf2f9571e3e19363b8d85f3a849cbf00f
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-04 21:05:19 -0700

    Add validation tests for resource category

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,12 +2,18 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
-  let(:resource_category) {ResourceCategory.new()}
+  let(:resource_category) {build(:resource_category)}
 
-  it "has a name" do 
-    expect(resource_category).to respond_to(:name)
+  describe "validations" do
+    it "has a name" do 
+      expect(resource_category).to respond_to(:name)
+    end
+
+    it { should validate_length_of(:name) }
+    it { should validate_uniqueness_of(:name).case_insensitive }
+
   end
-
+  
   describe "relationships" do
     it { should have_and_belong_to_many(:organizations) }
     it { should have_many(:tickets) }
