46972d12330eb7523c584df5295fa6eb153be80c
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-05 09:30:39 -0700

    Change organization spec validations to shoulda syntax

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -81,43 +81,20 @@ RSpec.describe Organization, type: :model do
   end 
 
   describe "validations" do  
-  	it "validates presence of email" do
-  	  expect(organization).to validate_presence_of(:email)
-  	end  
-  	it "validates presence of name" do
-  	  expect(organization).to validate_presence_of(:name)
-  	end  
-  	it "validates presence of phone" do
-  	  expect(organization).to validate_presence_of(:phone)
-  	end  
-  	it "validates presence of status" do
-  	  expect(organization).to validate_presence_of(:status)
-  	end  
-  	it "validates presence of primary_name" do
-  	  expect(organization).to validate_presence_of(:primary_name)
-  	end  
-  	it "validates presence of secondary_name" do
-  	  expect(organization).to validate_presence_of(:secondary_name)
-  	end  
-  	it "validates presence of secondary_phone" do
-  	  expect(organization).to validate_presence_of(:secondary_phone)
-  	end
-  	
-  	it "validates length of email" do
-  	  expect(organization).to validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create)
-  	end  
-  	it "validates uniqueness of email" do
-  	  expect(organization).to validate_uniqueness_of(:email).case_insensitive
-  	end  
-  	it "validates length of name" do
-  	  expect(organization).to validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
-  	end  
-  	it "validates uniqueness of name" do
-  	  expect(organization).to validate_uniqueness_of(:name).case_insensitive
-  	end  
-  	it "validates length of description" do
-  	  expect(organization).to validate_length_of(:description).is_at_most(1020).on(:create)
-  	end  
+
+  	it { should validate_presence_of(:email) } 
+  	it { should validate_presence_of(:name) }
+  	it { should validate_presence_of(:phone) }
+	it { should validate_presence_of(:status) }
+  	it { should validate_presence_of(:primary_name) }
+  	it { should validate_presence_of(:secondary_name) }
+  	it { should validate_presence_of(:secondary_phone) }
+	it { should validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create) }
+  	it { should validate_uniqueness_of(:email).case_insensitive }
+  	it { should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
+  	it { should validate_uniqueness_of(:name).case_insensitive }  
+  	it { should validate_length_of(:description).is_at_most(1020).on(:create) }
+ 
   end  
 
   describe "methods" do  
