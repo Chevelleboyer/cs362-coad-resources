34a34ae7f32a0a30cc8dcea659ddad7c98942381
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-04 20:57:00 -0700

    Add validation tests for region

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -4,12 +4,25 @@ RSpec.describe Region, type: :model do
 
   let(:region) {build(:region)}
 
-  it "has a name" do
-    expect(region).to respond_to(:name)
-  end
+  describe "validations" do
+    it "has a name" do
+      expect(region).to respond_to(:name)
+    end
 
+    it { should validate_length_of(:name) }
+    it { should validate_uniqueness_of(:name).case_insensitive }
+  
+  end
+  
   describe "relationships" do
     it { should have_many(:tickets) }
+  end
+
+  describe "methods" do 
+    it "responds to to_s with name" do
+      region_name = region.to_s
+      expect(region_name).to eq("FAKE")
+    end  
   end
 
 end
