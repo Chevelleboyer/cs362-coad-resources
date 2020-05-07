b7d29c2d7a9c4cb6d7dbc602132a10a2f8482b9b
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-07 09:05:35 -0700

    Add validation tests for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
     factory :user do
       email { "fake@email.com"}
       encrypted_password { "FAKEPASSWORD" }
+      role { 1 }
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) {build(:user)}
 
+  describe "validations" do
+    it { should validate_presence_of(:email)}
+  end
 
 end
