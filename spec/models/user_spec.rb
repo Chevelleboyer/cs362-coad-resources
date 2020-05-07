693e2686c583a05f17598f723d3138db71a44983
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-07 09:08:48 -0700

    Add to_s test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email)}
   end
 
+  describe "methods" do
+    it "responds to to_s with the user email" do
+      user_email = user.to_s
+      expect(user_email).to eq("fake@email.com")
+    end
+  end
+
 end
