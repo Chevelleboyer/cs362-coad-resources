dcb2c6fef5d23e338ff36857ddb0c793e0b53b9f
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-07 17:54:04 -0700

    Add passsword length validation test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe User, type: :model do
     it { should validate_length_of(:email) }
     it { should validate_uniqueness_of(:email).case_insensitive }
     it { should validate_presence_of(:password) }
+    it { should validate_length_of(:password) }
   end
 
   describe "relationships" do
