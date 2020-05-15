330ffe857bebf80b51ad81fdb5b29c89bcb4f495
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-14 18:51:39 -0700

    Add test for full title

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
+    
+    describe "#full_title" do
 
+      page_title = "FAKE"
+      base_title = "Disaster Resource Network"
+
+      it "returns: page_title | base_title" do
+        expect(full_title(page_title)).to eq("#{page_title} | #{base_title}")
+      end
+
+      it "returns: base_title, if page_title is empty" do
+        expect(full_title("")).to eq(base_title)
+      end
+    end
 end
