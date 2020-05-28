6d37070557539453881d2f59cd9e147ce6e13503
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-28 16:57:23 -0700

    Add create ticket spec

--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Ticket', type: :feature do
-
+  context "As an unregistered user" do
+    it "creates a new ticket" do
+      region = create(:region)
+      resource_category = create(:resource_category)
+      
+      visit(new_ticket_path)
+      fill_in("Full Name", with: "First Last")
+      fill_in("Phone Number", with: "5415081234")
+      select region.name, from: "Region"
+      select resource_category.name, from: "Resource Category"
+      fill_in("Description", with: "Description")
+      click_on("Send this help request")
+      expect(page).to have_content("Ticket Submitted")
+    end
+  end
 end
