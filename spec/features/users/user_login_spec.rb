27528369649bde333f09a504f5689790348ba960
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-28 16:38:22 -0700

    Move new user registration to appropriate spec file.

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -11,20 +11,4 @@ RSpec.describe 'Logging in', type: :feature do
       #expect(current_path).to eq(dashboard_path) 
     end
   end
-
-  context "as new user" do
-    it "lets you create an account with an email and password" do
-      new_email = "new@email.com"
-      new_password = "new_pass"
-      visit(new_user_registration_path)
-      fill_in("Email", with: new_email)
-      fill_in("Password", with: new_password)
-      fill_in("Password confirmation", with: new_password)
-      click_on("commit")
-      new_user = User.find_by(email: new_email)
-      visit(user_confirmation_path(confirmation_token: new_user.confirmation_token))
-      expect(page).to have_content("Your email address has been successfully confirmed")
-    end
-  end
-  
 end
--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'User registration', type: :feature do
-
+  context "as new user" do
+    it "lets you create an account with an email and password" do
+      new_email = "new@email.com"
+      new_password = "new_pass"
+      visit(new_user_registration_path)
+      fill_in("Email", with: new_email)
+      fill_in("Password", with: new_password)
+      fill_in("Password confirmation", with: new_password)
+      click_on("commit")
+      new_user = User.find_by(email: new_email)
+      visit(user_confirmation_path(confirmation_token: new_user.confirmation_token))
+      expect(page).to have_content("Your email address has been successfully confirmed")
+    end
+  end
 end
