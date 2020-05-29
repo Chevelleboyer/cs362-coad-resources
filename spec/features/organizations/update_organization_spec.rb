9ad629d3d17461243f2cccfd5151ff855b1971ce
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-29 00:13:38 -0700

    Add update organization spec

--- spec/features/organizations/update_organization_spec.rb
+++ spec/features/organizations/update_organization_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Updating an Organization', type: :feature do
+    let(:admin) { create(:user, :admin) }
+    let(:organization) { create(:organization, :approved) }
 
+    before do
+      create(:user, organization: organization)
+    end
+
+    context "as an admin" do
+      it "updates an organization" do
+        log_in_as(admin)
+        visit(organization_path(organization))
+        click_on("Edit Organization")
+        fill_in("Name", with: "Fake Name")
+        click_on("Update Resource")
+        expect(page).to have_content("Fake Name")
+      end
+    end
 end
