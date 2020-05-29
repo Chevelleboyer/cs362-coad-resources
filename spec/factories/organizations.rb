96029fc15daa71b082cde0814c9200c810d0609a
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-28 21:02:33 -0700

    Add additional parameters to Organizations factory

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :organization do
     name { "FAKE" }
+    primary_name { "Primary" }
+    secondary_name { "Secondary" }
+    email { "fake@email.com" }
+    phone { "5415081234" }
+    secondary_phone { "5415084321" }
+
+    trait :approved do
+      status { :approved }
+    end
   end
+
 end
