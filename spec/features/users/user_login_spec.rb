c0a732222e101056266c65fe7cb7fbffb5851272
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-30 12:33:46 -0700

    Add user login spec

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,15 +1,15 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'Logging in', type: :feature do
+RSpec.describe 'Logging in', type: :feature do
 
-#   context " as admin user" do
-#     it "Logs you in as admin" do
-#       admin = create(:user, :admin)
-#       log_in_as(admin)
-#       visit(dashboard_path)
-#       expect(current_path).to eq(dashboard_path) 
-#     end
+  context " as admin user" do
+    it "Logs you in as admin" do
+      admin = create(:user, :admin)
+      log_in_as(admin)
+      visit(dashboard_path)
+      expect(current_path).to eq(dashboard_path) 
+    end
 
-#   end
+  end
   
-# end
+end
