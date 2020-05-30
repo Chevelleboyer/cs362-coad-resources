18f243a6f9de0aa4585d3875a650609934a90bbe
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-30 15:35:52 -0700

    Add approved organization spec

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
+  let(:organization) { create(:organization, :submitted) }
+  let(:user) { create(:user, :admin) }
 
+  context "as an admin" do
+    it "approves an organization" do
+      log_in_as(user)
+      visit(organization_path(organization))
+      click_on("Approve")
+      expect(page).to have_content("#{organization.name} has been approved.")
+    end
+  end
 end
