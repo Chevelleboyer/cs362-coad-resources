bb3087ac52dee2ace2bb86bb71e26ba8d3712cb6
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-30 18:15:03 -0700

    Add admin deletes ticket spec

--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Ticket', type: :feature do
-
+  let(:resource_category) { create(:resource_category) }
+  let(:region) { create(:region) }
+  let(:ticket) { create(:ticket, region: region, resource_category: resource_category) }
+  let(:user) { create(:user, :admin) }
+  context "as an admin" do
+    it "deletes a ticket" do
+      log_in_as(user)
+      visit(ticket_path(ticket.id))
+      click_on("Delete")
+      expect(page).to have_content("Ticket #{ticket.id} was deleted.")
+    end
+  end
 end
