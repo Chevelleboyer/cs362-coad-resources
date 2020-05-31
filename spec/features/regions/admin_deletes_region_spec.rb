ed5375e6960d8d762267777d47ea5ea5e566d3b8
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-30 18:12:50 -0700

    Add admin deletes region spec

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting a Region', type: :feature do
 
+  let(:user) { create(:user, :admin) }
+  let(:region) { create(:region) }
+
+  context "as an admin" do
+    it "deletes a region" do
+      log_in_as(user)
+      visit(region_path(region.id))
+      click_on("Delete")
+      expect(page).to have_content("Region #{region.name} was deleted")
+    end
+  end
 end
