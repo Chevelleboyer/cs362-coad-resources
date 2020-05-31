152910bf16e79d3e7adb1e290e9b67b956ff2415
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-30 18:23:16 -0700

    Add admin deleted resource category spec

--- spec/features/resource_categories/admin_deleted_resource_category_spec.rb
+++ spec/features/resource_categories/admin_deleted_resource_category_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Resource Category', type: :feature do
+  let(:user) { create(:user, :admin) }
+  let(:resource_category) { create(:resource_category) }
 
+  context "as an admin" do
+    it "deletes a resource category" do 
+      log_in_as(user)
+      visit(resource_category_path(resource_category.id))
+      click_on("Delete")
+      expect(page).to have_content("Category #{resource_category.name} was deleted")
+    end
+  end
 end
