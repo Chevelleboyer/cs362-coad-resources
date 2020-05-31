7c4152ffd81ec288906e20d86f730c240ef97d1d
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-30 17:59:13 -0700

    Add reject organization spec

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
-
+  let(:organization) { create(:organization, :submitted) }
+  let(:user) { create(:user, :admin) }
+  
+  context "as an admin" do
+    it "rejects an organization" do
+      log_in_as(user)
+      visit(organization_path(organization))
+      fill_in("organization_rejection_reason", with: "Reason")
+      click_on("Reject")
+      expect(page).to have_content("#{organization.name} has been rejected.")
+    end
+  end
 end
