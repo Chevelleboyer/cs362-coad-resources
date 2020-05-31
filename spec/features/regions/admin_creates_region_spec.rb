e71f06b38d810cc28fd93f51b4ea377735aba77d
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-30 18:12:18 -0700

    Add admin creates region spec

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
-
+  
+  let(:user) { create(:user, :admin) }
+  
+  context "as an admin" do
+    it "creates a new region" do
+      log_in_as(user)
+      visit(new_region_path)
+      fill_in("Name", with: "Fake Region")
+      click_on("Add Region")
+      expect(page).to have_content("Region successfully created")
+    end
+  end
+  
 end
