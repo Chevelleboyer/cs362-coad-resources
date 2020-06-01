311ad4b1f24ba4bd94a16b2da047f09bcefbc1a2
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-31 17:51:47 -0700

    Add region controller test for index, non-admin user.

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
+  
+  let(:non_admin) { create(:user) }
+  let(:admin) { create(:user, :admin) }
 
+  context "as a non-admin user" do
+    before(:each) { sign_in(non_admin) }
+
+    describe "GET #index" do
+      specify { expect(get(:index)).to redirect_to(dashboard_path)}
+    end
+  end
 end
