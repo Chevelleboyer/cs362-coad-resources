69b23044f5bee8257f7170583321f54cb627b062
Chevelle Boyer <chevelleboyer@gmail.com>
2020-06-01 16:43:35 -0700

    Test that an unregistered user cannot submit a ticket with the name field left blank

--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Ticket', type: :feature do
   context "As an unregistered user" do
+
     it "creates a new ticket" do
       region = create(:region)
       resource_category = create(:resource_category)
@@ -15,5 +16,20 @@ RSpec.describe 'Creating a Ticket', type: :feature do
       click_on("Send this help request")
       expect(page).to have_content("Ticket Submitted")
     end
+
+    it "creates a new ticket with no name field" do
+      region = create(:region)
+      resource_category = create(:resource_category)
+
+      visit(new_ticket_path)
+      fill_in("Phone Number", with: "5415081234")
+      select region.name, from: "Region"
+      select resource_category.name, from: "Resource Category"
+      fill_in("Description", with: "Description")
+      click_on("Send this help request")
+      expect(page).not_to have_content("Ticket Submitted")
+    end
+    
   end
+
 end
