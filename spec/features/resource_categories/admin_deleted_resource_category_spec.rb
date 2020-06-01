759cca6134ed73bb4ce2f68aa865c2740043371f
Chevelle Boyer <chevelleboyer@gmail.com>
2020-06-01 16:25:30 -0700

    Fix indentation

--- spec/features/resource_categories/admin_deleted_resource_category_spec.rb
+++ spec/features/resource_categories/admin_deleted_resource_category_spec.rb
@@ -5,11 +5,14 @@ RSpec.describe 'Deleting a Resource Category', type: :feature do
   let(:resource_category) { create(:resource_category) }
 
   context "as an admin" do
+
     it "deletes a resource category" do 
       log_in_as(user)
       visit(resource_category_path(resource_category.id))
       click_on("Delete")
       expect(page).to have_content("Category #{resource_category.name} was deleted")
     end
+
   end
+
 end
