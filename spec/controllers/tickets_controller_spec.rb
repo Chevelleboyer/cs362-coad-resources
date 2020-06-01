24f00c41e6e15e8af10f948f24b33c112de426ad
Wyatt Pierce <piercewy@oregonstate.edu>
2020-05-31 21:43:19 -0700

    Add controller test for capture ticket.

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -21,6 +21,15 @@ RSpec.describe TicketsController, type: :controller do
         })).to redirect_to(ticket_submitted_path)
       end
     end
+
+    describe "GET #capture" do
+      it "captures a ticket" do
+        new_ticket = create(:ticket)
+        expect(get(:capture, :params => 
+        {:ticket => new_ticket,
+         :id => new_ticket.id})).to redirect_to(dashboard_path << "#tickets:open")
+      end
+    end
   end
 
   context "as an admin" do
