2247db9ccc7238b7f196b2119a49b0b5bc2d7998
Chevelle Boyer <chevelleboyer@gmail.com>
2020-06-01 16:57:48 -0700

    Test that a new user cannot sign up without an email

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'User registration', type: :feature do
+
   context "as new user" do
+
     it "lets you create an account with an email and password" do
       new_email = "new@email.com"
       new_password = "new_pass"
@@ -14,5 +16,17 @@ RSpec.describe 'User registration', type: :feature do
       visit(user_confirmation_path(confirmation_token: new_user.confirmation_token))
       expect(page).to have_content("Your email address has been successfully confirmed")
     end
+
+    it "leaves email field blank" do
+      new_password = "new_pass"
+
+      visit(new_user_registration_path)
+      fill_in("Password", with: new_password)
+      fill_in("Password confirmation", with: new_password)
+      click_on("commit")
+      expect(current_path).to eq("/users")
+    end
+
   end
+
 end
