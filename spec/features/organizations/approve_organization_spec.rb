6c840812f46cdee3ed0b8f1c19d5ffe645684c78
Chevelle Boyer <chevelleboyer@gmail.com>
2020-05-31 17:43:20 -0700

    Test that an admin is uunable to approve an approved organization

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -5,11 +5,22 @@ RSpec.describe 'Approving an organization', type: :feature do
   let(:user) { create(:user, :admin) }
 
   context "as an admin" do
+
     it "approves an organization" do
       log_in_as(user)
       visit(organization_path(organization))
       click_on("Approve")
       expect(page).to have_content("#{organization.name} has been approved.")
     end
+
+    it "approves an already approved organization" do
+      log_in_as(user)
+      visit(organization_path(organization))
+      click_on("Approve")
+      visit(organization_path(organization))
+      expect(page).not_to have_selector("Approve")
+    end
+
   end
+
 end
