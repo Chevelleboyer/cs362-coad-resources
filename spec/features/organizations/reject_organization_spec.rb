0581caaf17bf81f346312f2a54a19123f317463a
Chevelle Boyer <chevelleboyer@gmail.com>
2020-06-01 15:39:19 -0700

    Test that an already rejected organization application cannot be rejected again

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
+
   let(:organization) { create(:organization, :submitted) }
   let(:user) { create(:user, :admin) }
   
   context "as an admin" do
+
     it "rejects an organization" do
       log_in_as(user)
       visit(organization_path(organization))
@@ -12,5 +14,16 @@ RSpec.describe 'Rejecting an organization', type: :feature do
       click_on("Reject")
       expect(page).to have_content("#{organization.name} has been rejected.")
     end
+
+    it "rejects an approved organization" do
+      log_in_as(user)
+      visit(organization_path(organization))
+      fill_in("organization_rejection_reason", with: "Reason")
+      click_on("Reject")
+      visit(organization_path(organization))
+      expect(page).not_to have_selector("Reject")
+    end
+
   end
+
 end
