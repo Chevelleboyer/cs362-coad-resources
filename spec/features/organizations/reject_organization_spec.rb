050b9e6aa9f6ef320f150f951c107d07e4ca2bc8
Chevelle Boyer <chevelleboyer@gmail.com>
2020-06-01 15:40:57 -0700

    Test that an already approved organization application cannot be rejected

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -15,11 +15,19 @@ RSpec.describe 'Rejecting an organization', type: :feature do
       expect(page).to have_content("#{organization.name} has been rejected.")
     end
 
-    it "rejects an approved organization" do
+    it "rejects a rejected organization" do
       log_in_as(user)
       visit(organization_path(organization))
       fill_in("organization_rejection_reason", with: "Reason")
       click_on("Reject")
+      visit(organization_path(organization))
+      expect(page).not_to have_selector("Reject")
+    end
+
+    it "rejects an approved organization" do
+      log_in_as(user)
+      visit(organization_path(organization))
+      click_on("Approve")
       visit(organization_path(organization))
       expect(page).not_to have_selector("Reject")
     end
